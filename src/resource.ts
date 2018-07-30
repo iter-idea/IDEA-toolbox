f4f8113b1e92d906d9fc6d599475d913e8e9b88e
MC <mc@matteocarbone.com>
2018-07-30 14:43:13 +0200

    v2.2.2 - Resource: added keys

--- src/resource.ts
+++ src/resource.ts
@@ -4,14 +4,19 @@
 export abstract class Resource {
   /**
    * Object initialization, with a bulk set of its attributes.
-   * @param {any} attributes
+   * @param {any} attributes the attributes, except the keys
+   * @param {any} keys the keys to set
    */
-  constructor(attributes?: any) {
+  constructor(attributes?: any, keys?: any) {
+    if(keys) {
+      // e.g. this.keyAttr = keys.keyAttr || null;
+      // ...
+    }
     attributes = attributes || {};
     // e.g. this.attr = attributes.attr || null;
     // ...
   }
-
+  
   /**
    * Valide the object's attributes, performing all the checkings.
    * @return {Array<string>} errors; if empty, the checkings are passed
