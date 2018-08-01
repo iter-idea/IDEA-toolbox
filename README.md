27e39afc76aec082fa4304a52f51526226aceb0e
MC <mc@matteocarbone.com>
2018-08-01 10:27:19 +0200

    idea-toolbox <> idea-aws

--- README.md
+++ README.md
@@ -20,9 +20,3 @@ import Idea = require('idea-toolbox');
 ## Documentation
 
 Documentation generated with TypeDoc: [link](https://uatisdeproblem.github.io/IDEA-toolbox).
-
-## Notes
-
-- The AWS-SDK is a back-end only package, which is already pre-installed in any Lambda Function;
-therefore, we included it as devDependency. This to avoid errors on client side, due to some 
-webpackage missing.
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-export { Resource } from './resource';
-export { ResourceController, ResourceControllerOptions } from './resourceController';
-export { DynamoDB } from './dynamoDB';
-export { Cognito } from './cognito';
-export { APIGateway} from './apiGateway';
-export { S3 } from './s3';
-export { SES } from './ses';
-export { SNS } from './sns';
 export { Utils } from './utils';
+export { Resource } from './resource';
