c25ee4a64b8369f643f4cf3239f11e22b79c6114
uatisdeproblem <mc@matteocarbone.com>
2019-10-03 11:28:02 +0200

    Improved Notification

--- src/notification.model.ts
+++ src/notification.model.ts
@@ -1,28 +1,66 @@
-export interface Notification {
+/**
+ * An object to pass to SNS topics that manage notifications.
+ */
+export class Notification {
+  /**
+   * The project from which we fire the notification.
+   */
+  public project: string;
   /**
    * The id of the team owner of the notification. Useful for branded notifications.
    */
-  teamId?: string;
+  public teamId?: string;
   /**
    * The id of the user receiver.
    * Either userId or email must be set.
    */
-  userId?: string;
+  public userId?: string;
   /**
    * In case the user doesn't exist (!userId), the email address to which send an email notification.
    * Either email or userId must be set.
    */
-  email?: string;
+  public email?: string;
   /**
    * A brief description of the notification.
    */
-  subject: string;
+  public subject: string;
   /**
    * The content of the notification; markdown support.
    */
-  content: string;
+  public content: string;
   /**
    * If set, an email notification is preferred to any other channel.
    */
-  forceEmail?: boolean;
+  public forceEmail?: boolean;
+
+  constructor(x?: Notification | any) {
+    x = (x || {}) as Notification;
+    this.project = x.project ? String(x.project) : null;
+    this.teamId = x.teamId ? String(x.teamId) : null;
+    this.userId = x.userId ? String(x.userId) : null;
+    this.email = x.email ? String(x.email) : null;
+    this.subject = x.subject ? String(x.subject) : null;
+    this.content = x.content ? String(x.content) : null;
+    this.forceEmail = Boolean(x.forceEmail);
+  }
+}
+
+/**
+ * Preferences for receiving notifications.
+ */
+export class NotificationsPreferences {
+  /**
+   * If true, always send an email.
+   */
+  public email: boolean;
+  /**
+   * If true, always send a push notification.
+   */
+  public push: boolean;
+
+  constructor(x?: NotificationsPreferences | any) {
+    x = (x || {}) as NotificationsPreferences;
+    this.email = Boolean(x.email);
+    this.push = Boolean(x.push);
+  }
 }
