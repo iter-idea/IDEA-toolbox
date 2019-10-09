b56af4dbd68052a1e4c63307a629ffe1d006517f
uatisdeproblem <mc@matteocarbone.com>
2019-10-09 17:37:20 +0200

    Added Languages

---
+++ src/languages.model.ts
@@ -0,0 +1,31 @@
+import { Resource } from './resource.model';
+
+export class Languages extends Resource {
+  /**
+   * The default language for a context.
+   */
+  public default: string;
+  /**
+   * The available languages available in a context.
+   */
+  public available: Array<string>;
+
+  constructor(x?: Languages | any) {
+    super();
+    this.default = 'en';
+    this.available = ['en'];
+    if (x) this.load(x);
+  }
+
+  public load(x: any) {
+    this.default = this.clean(x.default, String);
+    this.available = this.cleanArray(x.available, String);
+  }
+
+  public validate() {
+    const e = super.validate();
+    if (!this.default) e.push('default');
+    if (!this.available.some(x => x === this.default)) e.push('available');
+    return e;
+  }
+}
