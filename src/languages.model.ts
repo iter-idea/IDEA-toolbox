48414cd5e6b228bc508f18b9a4e061f9f4245ab9
uatisdeproblem <mc@matteocarbone.com>
2020-05-04 15:23:47 +0200

    Improved Languages

--- src/languages.model.ts
+++ src/languages.model.ts
@@ -1,25 +1,36 @@
 import { Resource } from './resource.model';
+import { loopStringEnumValues } from './utils';
 
+/**
+ * Structure representing language preferences in a context.
+ */
 export class Languages extends Resource {
   /**
    * The default language for a context (e.g. 'it', 'en', etc.).
    */
-  public default: string;
+  public default: ServiceLanguages;
   /**
    * The available languages available in a context (e.g. 'it', 'en', etc.).
    */
-  public available: Array<string>;
+  public available: Array<ServiceLanguages>;
 
   public load(x: any) {
     super.load(x);
-    this.default = this.clean(x.default, String, 'en');
-    this.available = this.cleanArray(x.available, String, ['en']);
+    this.default = this.clean(x.default, String, ServiceLanguages.English);
+    this.available = this.cleanArray(x.available, String, [ServiceLanguages.English]);
   }
 
   public validate() {
     const e = super.validate();
-    if (!this.default) e.push('default');
-    if (!this.available.some(x => x === this.default)) e.push('available');
+    if (!this.default || !this.available.some(x => x === this.default)) e.push('default');
+    if (!this.available.every(l => loopStringEnumValues(ServiceLanguages).some(x => x === l))) e.push('available');
     return e;
   }
+}
+
+/**
+ * The languages available in a specific service. It must be redefined in every service.
+ */
+export declare enum ServiceLanguages {
+  English = 'en'
 }
