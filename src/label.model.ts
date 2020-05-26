57e9fdc5c089d5c93d032f7290a906eb2baebe6d
uatisdeproblem <mc@matteocarbone.com>
2020-05-26 15:13:56 +0200

    Added LabelVariable

--- src/label.model.ts
+++ src/label.model.ts
@@ -36,3 +36,17 @@ export class Label {
     return this[language] || this[languages.default];
   }
 }
+
+/**
+ * A variable to substitute at runtime.
+ */
+export interface LabelVariable {
+  /**
+   * The code of the variable (the string that will be substituted).
+   */
+  code: string;
+  /**
+   * The description of the variable, i.e. the explanation of the content that will be substituted to this variable.
+   */
+  description: string;
+}
