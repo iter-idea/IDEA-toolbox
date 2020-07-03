ef34732ac4331313f197303e7e0f845a40bdecae
uatisdeproblem <mc@matteocarbone.com>
2020-07-03 10:03:28 +0200

    Fixed emailData constructor (to, cc, bcc)

--- src/emailData.model.ts
+++ src/emailData.model.ts
@@ -26,9 +26,10 @@ export class EmailData extends Resource {
     super.load(x);
     this.subject = this.clean(x.subject, String);
     this.content = this.clean(x.content, String);
-    this.to = typeof x.to === 'string' ? this.clean(x.to, String).split(',') : this.cleanArray(x.to, String);
-    this.cc = typeof x.cc === 'string' ? this.clean(x.cc, String).split(',') : this.cleanArray(x.cc, String);
-    this.bcc = typeof x.bcc === 'string' ? this.clean(x.bcc, String).split(',') : this.cleanArray(x.bcc, String);
+    this.to = x.to && typeof x.to === 'string' ? this.clean(x.to, String).split(',') : this.cleanArray(x.to, String);
+    this.cc = x.cc && typeof x.cc === 'string' ? this.clean(x.cc, String).split(',') : this.cleanArray(x.cc, String);
+    this.bcc =
+      x.bcc && typeof x.bcc === 'string' ? this.clean(x.bcc, String).split(',') : this.cleanArray(x.bcc, String);
   }
 }
 
