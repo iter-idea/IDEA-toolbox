5a89779229660a7e3d75f7f0be7ac354d7e4bdf1
uatisdeproblem <mc@matteocarbone.com>
2020-08-01 16:45:27 +0200

    LimitCounter -> ProjectSubscriptionLimit

--- src/projectSubscriptionLimit.model.ts
+++ src/projectSubscriptionLimit.model.ts
@@ -1,22 +1,45 @@
 import { Resource } from './resource.model';
+import { epochDateTime } from './epoch';
 
 /**
- * A limit counter.
+ * The current limits of a project subscription.
+ * Note: this model should be updated only with direct updates (not with entire PUTs).
+ *
+ * Table: `idea_projectsSubscriptions_limits`.
+ *
+ * Indexes:
+ *   - `projectSubscriptionId-renewsOn-index` LSI, all.
  */
-export class LimitCounter extends Resource {
+export class ProjectSubscriptionLimit extends Resource {
   /**
-   * The counter.
+   * The concatenation of a project and the id of a subscription.
+   */
+  public projectSubscriptionId: string;
+  /**
+   * The id of the limit.
+   */
+  public limitId: string;
+  /**
+   * The numeric representation of the limit.
+   */
+  public limit: number;
+  /**
+   * The counter towards the limit.
    */
   public counter: number;
   /**
-   * The limit against which the counter moves.
+   * The timestamp of when a renewable limit will be renewed (the counter will be reset).
+   * If not set, the limit isn't renewable.
    */
-  public limit: number;
+  public renewsOn?: epochDateTime;
 
   public load(x: any) {
     super.load(x);
-    this.counter = this.clean(x.counter, Number);
+    this.projectSubscriptionId = this.clean(x.projectSubscriptionId, String);
+    this.limitId = this.clean(x.limitId, String);
     this.limit = this.clean(x.limit, Number);
+    this.counter = this.clean(x.counter, Number);
+    if (x.renewsOn) this.renewsOn = this.clean(x.renewsOn, a => new Date(a).getTime());
   }
 
   /**
