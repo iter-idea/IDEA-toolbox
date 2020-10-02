963c1a21aa3b3f1126323c86e4e6634be9689e30
uatisdeproblem <mc@matteocarbone.com>
2020-10-02 12:37:11 +0200

    Error reporting: fixed expiresAt calculation

--- src/errorReport.model.ts
+++ src/errorReport.model.ts
@@ -3,6 +3,8 @@ import { epochDateTime } from './epoch';
 import { ClientInfo } from './clientInfo.model';
 import { ClientError } from './clientError.model';
 
+const EXPIRES_AT_DEFAULT_MARING_SEC = 5184000; // 60 days
+
 /**
  * Table: `idea_projects_errorsReports`.
  */
@@ -32,7 +34,7 @@ export class ErrorReport extends Resource {
     super.load(x);
     this.project = this.clean(x.project, String);
     this.createdAt = this.clean(x.createdAt, a => new Date(a), Date.now());
-    this.expiresAt = Math.round(new Date(this.createdAt).getTime() / 1000);
+    this.expiresAt = Math.round(new Date(this.createdAt).getTime() / 1000 + EXPIRES_AT_DEFAULT_MARING_SEC);
     this.error = new ClientError(x.error);
     this.client = new ClientInfo(x.client);
   }
@@ -41,6 +43,6 @@ export class ErrorReport extends Resource {
     this.safeLoad(newData, safeData);
     this.project = safeData.project;
     this.createdAt = safeData.createdAt;
-    this.expiresAt = safeData.expiresAt;
+    this.expiresAt = Math.round(safeData.createdAt / 1000 + EXPIRES_AT_DEFAULT_MARING_SEC);
   }
 }
