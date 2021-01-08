4a9f7f4098af04db597650afe4ef431e9de68f24
Matteo Carbone <mc@matteocarbone.com>
2021-01-08 19:47:25 +0100

    Reversed webpack (it conflicts with front-end)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # remove the old dist folder
 rm -rf ./dist
-# compile and pack
-npx webpack -o ./dist --progress
+# compile
+tsc
 # generate documentation
 npx typedoc --out ./docs ./src --theme minimal --mode file --exclude /**/index.ts --excludePrivate --excludeExternals
