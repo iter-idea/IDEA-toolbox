ee6b203f2153c57f09c61c127940b820c70c25d3
Matteo Carbone <mc@matteocarbone.com>
2021-09-20 10:02:21 +0200

    chore: update to latest standards

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-# remove the old dist folder
-rm -rf ./dist
-# compile
-tsc
+# other parameters
+C='\033[4;32m' # color
+NC='\033[0m'   # reset (no color)
+
+# set the script to exit in case of errors
+set -o errexit
+
+# lint the code in search for errors
+echo -e "${C}Linting...${NC}"
+npm run lint ${SRC_FOLDER} 1>/dev/null
+
+# compile the project's typescript code
+echo -e "${C}Compiling...${NC}"
+npm run compile 1>/dev/null
+
 # generate documentation
-npx typedoc --out ./docs ./src --theme minimal --mode file --exclude /**/index.ts --excludePrivate --excludeExternals
+echo -e "${C}Documenting...${NC}"
+npm run docs index.ts 1>/dev/null
