e617cebbb3cc55f948eaee7bcbacb309ca1eca6a
Matteo Carbone <mc@matteocarbone.com>
2021-10-27 17:19:33 +0200

    feat(CognitoUser): custom attributes

--- src/cognitoUser.model.ts
+++ src/cognitoUser.model.ts
@@ -1,43 +1,40 @@
-import { Resource } from './resource.model';
-
 /**
  * A user stored in a Cognito User Pool.
  */
-export class CognitoUser extends Resource {
+export class CognitoUser {
   /**
    * The id of the user.
    */
   userId: string;
   /**
-   * The fullname of the user.
-   */
-  name: string;
-  /**
    * The email address of the user.
    */
   email: string;
   /**
+   * The fullname of the user.
+   */
+  name: string;
+  /**
    * The list of groups containing the user.
    */
   groups: string[];
-
   /**
-   * Load the attributes from an already existing resource.
-   * Note: the method also reads from the user's token (claims) format.
+   * The user's custom attributes.
    */
-  load(x: any) {
-    super.load(x);
-    this.userId = this.clean(x.userId || x.sub, String);
-    this.name = this.clean(x.name, String);
-    this.email = this.clean(x.email, String);
-    this.groups = this.cleanArray(x.groups || x['cognito:groups']?.split(','), String);
-  }
+  custom: { [attribute: string]: string | number };
 
-  validate(): string[] {
-    const e = super.validate();
-    if (this.iE(this.name)) e.push('name');
-    if (this.iE(this.email)) e.push('email');
-    return e;
+  constructor(x: any = {}) {
+    this.userId = x.userId || x.sub;
+    this.email = x.email;
+    this.name = x.name;
+    this.groups = x.groups || x['cognito:groups']?.split(',');
+    if (x.custom) this.custom = x.custom;
+    else {
+      this.custom = {};
+      Object.keys(x)
+        .filter(a => a.startsWith('custom:'))
+        .forEach(a => (this.custom[a.slice('custom:'.length + 1)] = x[a]));
+    }
   }
 
   /**
