f219b7bbe2d5317e39f13b5f9cb3db77583459b6
Matteo Carbone <mc@matteocarbone.com>
2022-09-21 10:54:18 +0200

    chore: refresh utils

--- src/utils.ts
+++ src/utils.ts
@@ -8,45 +8,15 @@ import { marked } from 'marked';
 import { markdown } from './markdown';
 import { epochISODateString } from './epoch';
 
-/**
- * Utilities (static) functions, to support IDEA's projects.
- */
+//
+// Utilities (static) functions, to support IDEA's projects.
+//
 
 /**
- * Convert an ISODate string to the Italian format.
- * @param ds new Date().toISOString();
- * @returns cleaned date
- * @deprecated
+ * Parse a date in the format `YYYY-MM-DD`.
  */
-export function ISODateToItalianFormat(ds: string): string {
-  return `${ds.slice(8, 10)}/${ds.slice(5, 7)}/${ds.slice(0, 4)}`;
-}
-/**
- * Parse a date in the format YYYY-MM-DD.
- */
-export function toISODate(date: Date | string | number): epochISODateString {
-  if (!date) return null;
-  return new Date(date).toISOString().slice(0, 10);
-}
-
-/**
- * Convert a date to the specified locale format.
- * @param date the date to convert
- * @param lang two-letter code (e.g. 'en' or 'it')
- * @param short if true, get the short version of the date
- * @param noYear if true, don't concat the year
- * @returns the date converted
- * @deprecated
- */
-export function dateToLocale(date: Date, lang: string, short?: boolean, noYear?: boolean): string {
-  let dayName = date.toLocaleDateString(lang, { weekday: short ? 'short' : 'long' });
-  dayName = dayName.slice(0, 1).toUpperCase().concat(dayName.slice(1));
-  const day = date.toLocaleDateString(lang, { day: 'numeric' });
-  let month = date.toLocaleDateString(lang, { month: short ? 'short' : 'long' });
-  month = month.slice(0, 1).toUpperCase().concat(month.slice(1));
-  const year = date.toLocaleDateString(lang, { year: 'numeric' });
-  return `${dayName} ${day} ${month} ${noYear ? '' : year}`;
-}
+export const toISODate = (date: Date | string | number): epochISODateString =>
+  !date ? null : new Date(date).toISOString().slice(0, 10);
 
 /**
  * Clean a string to use it within filenames and so.
@@ -54,9 +24,8 @@ export function dateToLocale(date: Date, lang: string, short?: boolean, noYear?:
  * @param separator optional, separator char
  * @returns cleaned string
  */
-export function cleanStr(str: string, separator?: string): string {
-  return (str || '').toLowerCase().replace(/[^a-zA-Z0-9]+/g, separator || '');
-}
+export const cleanStr = (str: string, separator?: string): string =>
+  (str ?? '').toLowerCase().replace(/[^a-zA-Z0-9]+/g, separator ?? '');
 
 /**
  * Join two arrays by a common column, selecting which data to extract.
@@ -69,41 +38,37 @@ export function cleanStr(str: string, separator?: string): string {
  * function are ignored
  * @returns the joined array
  */
-export function joinArraysOnKeys(
+export const joinArraysOnKeys = (
   mainTable: any[],
   lookupTable: any[],
   mainKey: string,
   lookupKey: string,
   selectFunction: (attrMainTable: string, attrLookupTable: string) => any[]
-): any[] {
-  const l = lookupTable.length;
-  const m = mainTable.length;
+): any[] => {
   const lookupIndex = [];
   const output = [];
-  for (let i = 0; i < l; i++) {
-    // loop through l items
-    const row = lookupTable[i];
-    lookupIndex[row[lookupKey]] = row; // create an index for lookup table
-  }
-  for (let j = 0; j < m; j++) {
-    // loop through m items
-    const y = mainTable[j];
-    const x = lookupIndex[y[mainKey]]; // get corresponding row from lookupTable
-    const o = selectFunction(y, x); // select only the columns you need
-    if (o) output.push(o); // null values returned by the select function are ignored
+  for (const row of lookupTable) lookupIndex[row[lookupKey]] = row;
+  for (const row of mainTable) {
+    const lookupTableRow = lookupIndex[row[mainKey]];
+    const outputElement = selectFunction(row, lookupTableRow);
+    if (outputElement) output.push(outputElement);
   }
   return output;
-}
+};
 
 /**
  * Check if a field (/variable) is empty or invalid, based on its type.
  * If the type isn't passed as a parameter, it will be auto-detected.
  * @param field the field to check
- * @param type set to force a type check; enum: string, number, boolean, object, date, email, phone, url, domain
+ * @param fieldType set to force a type check
  * @returns return if the field is empty/invalid or not
  */
-export function isEmpty(field: any, type: string = typeof field): boolean {
+export const isEmpty = (
+  field: any,
+  fieldType?: 'string' | 'number' | 'boolean' | 'object' | 'date' | 'email' | 'phone' | 'url' | 'domain'
+): boolean => {
   if (field === null || field === undefined) return true;
+  const type = fieldType ?? typeof field;
   if (!type) return true;
   switch (type) {
     case 'string':
@@ -130,67 +95,46 @@ export function isEmpty(field: any, type: string = typeof field): boolean {
     default:
       return true;
   }
-}
-
-/**
- * Add a formatted log in the console.
- * @param context context in which the content apply
- * @param err error
- * @param content the content to log
- * @param important optional; if true, highlight the line in CloudWatch
- * @deprecated use IDEA-AWS Logger class instead.
- */
-export function logger(context: string, err?: Error | any, content?: any, important?: boolean) {
-  const someContent = content !== undefined && content !== null;
-  // eslint-disable-next-line no-console
-  if (err) console.error('[ERROR]', '≫', err, someContent ? content : '');
-  else if (important) console.log(`[${context}]`, someContent ? content : '');
-  // to give less importance to debug info
-  else console.log('.....', context, someContent ? '≫ '.concat(content) : '');
-}
-
-/**
- * Get an array to iterate containing the keys of a string enum.
- */
-export function loopStringEnumKeys(theEnum: any): string[] {
-  return Object.keys(theEnum);
-}
-/**
- * Get an array to iterate containing the values of a string enum.
- */
-export function loopStringEnumValues(theEnum: any): string[] {
-  return Object.keys(theEnum).map(key => theEnum[key]);
-}
-/**
- * Get an array to iterate containing the keys of a numeric enum.
- */
-export function getStringEnumKeyByValue(theEnum: any, value: string): string {
-  const el = Object.keys(theEnum)
-    .map(key => ({ value: theEnum[key], key }))
-    .find(x => x.value === value);
-  return el ? el.key : null;
-}
-/**
- * Get an array to iterate containing the keys of a numeric enum.
- */
-export function loopNumericEnumKeys(theEnum: any): number[] {
-  return Object.keys(theEnum)
-    .filter(key => !isNaN(Number(key)))
-    .map(c => Number(c));
-}
-/**
- * Get an array to iterate containing the values of a numeric enum.
- */
-export function loopNumericEnumValues(theEnum: any): string[] {
-  return Object.keys(theEnum)
-    .filter(key => !isNaN(Number(theEnum[key])))
-    .map(c => String(c));
-}
+};
 
 /**
  * Convert a markdown string to HTML.
  */
-export function mdToHtml(mdString: markdown | string): string {
-  if (!mdString) return '';
-  return marked(mdString, { gfm: true, breaks: true });
-}
+export const mdToHtml = (mdString: markdown | string): string =>
+  !mdString ? '' : marked(mdString, { gfm: true, breaks: true });
+
+//
+// DEPRECATED
+//
+
+/**
+ * @deprecated Get an array to iterate containing the keys of a string enum.
+ */
+export const loopStringEnumKeys = (theEnum: any): string[] => Object.keys(theEnum);
+/**
+ * @deprecated Get an array to iterate containing the values of a string enum.
+ */
+export const loopStringEnumValues = (theEnum: any): string[] => Object.keys(theEnum).map(key => theEnum[key]);
+/**
+ * @deprecated Get an array to iterate containing the keys of a numeric enum.
+ */
+export const getStringEnumKeyByValue = (theEnum: any, value: string): string => {
+  const el = Object.keys(theEnum)
+    .map(key => ({ value: theEnum[key], key }))
+    .find(x => x.value === value);
+  return el ? el.key : null;
+};
+/**
+ * @deprecated Get an array to iterate containing the keys of a numeric enum.
+ */
+export const loopNumericEnumKeys = (theEnum: any): number[] =>
+  Object.keys(theEnum)
+    .filter(key => !isNaN(Number(key)))
+    .map(c => Number(c));
+/**
+ * @deprecated Get an array to iterate containing the values of a numeric enum.
+ */
+export const loopNumericEnumValues = (theEnum: any): string[] =>
+  Object.keys(theEnum)
+    .filter(key => !isNaN(Number(theEnum[key])))
+    .map(c => String(c));
