0e9e0a7a3e81245cf2be2fc81630daf1407b131c
Matteo Carbone <mc@matteocarbone.com>
2024-12-24 08:57:25 +0000

    feat(attachments): attachment sections

--- src/attachment.model.ts
+++ src/attachment.model.ts
@@ -1,19 +1,21 @@
+import { Label } from './label.model';
+import { Languages } from './languages.model';
 import { Resource } from './resource.model';
 
 /**
- * An attachment to attach to a resource.
+ * An attachment to stick to an entity.
  */
 export class Attachment extends Resource {
   /**
-   * The id of the attachment.
+   * The ID of the attachment.
    */
   attachmentId: string;
   /**
-   * The name of the resource. This can be changed when attaching to the resource.
+   * The name of the attachment.
    */
   name: string;
   /**
-   * The format of the resource. (e.g. 'jpg', 'pdf').
+   * The format of the attachment, without a prefixing dot (e.g. 'jpg', 'pdf').
    */
   format: string;
 
@@ -35,6 +37,78 @@ export class Attachment extends Resource {
     if (this.iE(this.attachmentId)) e.push('attachmentId');
     if (this.iE(this.name)) e.push('name');
     if (this.iE(this.format)) e.push('format');
+    return e;
+  }
+
+  /**
+   * Get the filename (`name.format`) of the attachment.
+   */
+  getFilename(): string {
+    return this.name.concat('.', this.format);
+  }
+}
+
+/**
+ * A section to group a list of attachments.
+ */
+export class AttachmentSection extends Resource {
+  /**
+   * The name of the section (multilanguage).
+   */
+  name: Label;
+  /**
+   * The description of the section (multilanguage).
+   */
+  description: Label;
+  /**
+   * The list of attachments in the section.
+   */
+  attachments: Attachment[];
+
+  load(x: any, languages: Languages): void {
+    super.load(x);
+    this.name = new Label(x.name, languages);
+    this.description = new Label(x.description, languages);
+    this.attachments = this.cleanArray(x.attachments, a => new Attachment(a));
+  }
+
+  validate(languages: Languages): string[] {
+    const e = super.validate();
+    if (this.name.validate(languages).length) e.push('name');
+    return e;
+  }
+}
+
+/**
+ * A block of reorderable sections each containing a list of attachments.
+ * Use it when you need to organize the attachments in sections or categories.
+ */
+export class AttachmentSections extends Resource {
+  /**
+   * Ordered list of the sections (keys) to expect in the block.
+   * Example: `['flowers', 'burgers', ...]`.
+   */
+  sectionsLegend: string[];
+  /**
+   * Object containg attributes of type AttachmentSection; e.g.
+   * ```
+   * sections.flowers: AttachmentSection;
+   * sections.burgers: AttachmentSection;
+   * ...
+   * ```
+   */
+  sections: Record<string, AttachmentSection>;
+
+  load(x: any, languages: Languages): void {
+    super.load(x, languages);
+    this.sectionsLegend = this.cleanArray(x.sectionsLegend, String);
+    this.sections = {};
+    this.sectionsLegend.forEach(s => (this.sections[s] = new AttachmentSection(x.sections[s], languages)));
+  }
+
+  validate(languages: Languages): string[] {
+    const e = super.validate();
+    this.sectionsLegend.forEach(s => this.sections[s].validate(languages).forEach(es => e.push(`${s}.${es}`)));
     return e;
   }
 }
